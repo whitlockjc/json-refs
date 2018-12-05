f50f2f1062c1a31dc980dbbcbfbe24d6f8c6d7fc
Brett Zamir <brettz9@yahoo.com>
2018-12-05 21:14:01 +0800

    - Build: Update to Babel 7 and away from deprecated yearly ES2015 to @babel/preset-env - npm: Avoid lodash as devDep since already dep

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = [{
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['es2015']
+            presets: ['@babel/env']
           }
         }
       }
@@ -38,7 +38,7 @@ module.exports = [{
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['es2015']
+            presets: ['@babel/env']
           }
         }
       }
