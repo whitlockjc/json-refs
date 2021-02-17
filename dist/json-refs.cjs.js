b1bab3ff9ec444068d472780e0727b0e08fa7e3a
Chris Armstrong <chris-armstrong@users.no-reply.github.com>
2021-02-18 09:29:42 +1100

    remove committed dist/ folder

--- dist/json-refs.cjs.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./json-refs.cjs.prod.js");
-} else {
-  module.exports = require("./json-refs.cjs.dev.js");
-}
